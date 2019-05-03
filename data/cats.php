d00b966bd323e6db2012130193760d5e462615f9
uknbr <ukn_linux@yahoo.com.br>
2019-05-03 15:37:26 -0300

    change Adelaide cat description

--- data/cats.php
+++ data/cats.php
@@ -15,7 +15,7 @@ $_SESSION['item'] = array();
 $_SESSION['item'][0]['name'] = 'adelaide';
 $_SESSION['item'][0]['rank'] = 1;
 $_SESSION['item'][0]['caption'] = 'Adelaide Cat';
-$_SESSION['item'][0]['trivia'] = "<p>My name is Le Cornu and I live in Adelaide. My dad plays for the Adelaide Crows. He has a big mullet which I snuggle into when he is asleep.</p> <p style='color:silver;font-size:80%;font-style:italic'>Like me and we can watch the footie together. </p>";
+$_SESSION['item'][0]['trivia'] = "<p>My name is Le Cornu and I live in Adelaide. My mom plays for the Adelaide Crows. He has a big mullet which I snuggle into when he is asleep.</p> <p style='color:silver;font-size:80%;font-style:italic'>Like me and we can watch the footie together. </p>";
 $_SESSION['item'][0]['theme'] = 'cats';
 $_SESSION['item'][0]['filename'] = 'data/images/cats/adelaide.jpg';
 $_SESSION['item'][0]['rating'] = 0;
